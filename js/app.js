dd4696fc5c7efca95b00501476f35fd858a962f2
AlenFarky <farky.dev@gmail.com>
2022-11-03 00:58:34 +0100

    Update app.js

--- js/app.js
+++ js/app.js
@@ -15,7 +15,7 @@ let marker;
 L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png").addTo(map);
 
 async function trackIp() {
-  const apiKey = "at_GUTqH4sC2vYT8wrpd18PFEuIRr2JC";
+  const apiKey = "at_XUqYZhpTeJ3vZhs72WSjsHci36Rwr";
   const query = document.querySelector(".input-container input").value;
 
   if (query === "") {
