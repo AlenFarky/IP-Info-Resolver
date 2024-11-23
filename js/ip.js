9e83f69bbd65ca947bcd9ee6627be1dfd03615a1
AlenFarky <farky.dev@gmail.com>
2024-11-23 16:29:30 +0100

    Update ip.js

--- js/ip.js
+++ js/ip.js
@@ -1,6 +1,11 @@
-$.getJSON("https://api.ipify.org?format=json",
-                                  function(data) {
-
+$.getJSON("https://api.ipify.org?format=json", function(data) {
+    const ipElement = $("#ip");
+    ipElement.html(data.ip);
     
-    $("#ip").html(data.ip);
-})
+    // Dodaj event listener za klik na IP
+    ipElement.on('click', function() {
+        // Ubaci IP u input polje i pokreni handler
+        inputField.value = data.ip;
+        handler(new Event('submit'));  // Ručno kreiraj event i proslijedi ga handleru
+    });
+});
